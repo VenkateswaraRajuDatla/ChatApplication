abd6b611a4b019564b1bbc42f8753017b81efd5d
Venkateswara Raju Datla <dvrajudatla1995@gmail.com>
2018-08-05 01:12:42 -0700

    Add geolocation support via geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -12,10 +12,17 @@ socket.on('newMessage', function (message) {
   console.log('newMessage', message);
   var li=jQuery('<li></li>');
   li.text(`${message.from}: ${message.text}`);
-
   jQuery('#messages').append(li);
 });
 
+socket.on('newLocationMessage',function(message){
+  var li=jQuery('<li></li>');
+  var a=jQuery('<a target="_blank">My current location</a>');
+  li.text(`${message.from}: `);
+  a.attr('href',message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+});
 
 jQuery('#message-form').on('submit',function(e){
   e.preventDefault();
@@ -25,5 +32,23 @@ jQuery('#message-form').on('submit',function(e){
     text:jQuery('[name=message]').val()
   },function(){
 
+  });
+});
+
+var locationbutton =jQuery('#send-location');
+locationbutton.on('click',function(){
+  if(!navigator.geolocation)
+  {
+    return alert('geolocation is not supported by browser');
+  }
+
+  navigator.geolocation.getCurrentPosition(function(position){
+    console.log(position);
+    socket.emit('createLocationMessage',{
+      latitude:position.coords.latitude,
+      longitude:position.coords.longitude
+    });
+  },function(){
+    alert('unable to fetch current position');
   });
 });
